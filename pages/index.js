cb4ddd36ffb438acec3f4c151b78c58e95dd47ed
adon94 <adambfad@gmail.com>
2022-10-21 14:22:53 +0100

    add id select and only pdf option

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import { useEffect, useRef, useState } from 'react';
+import { useRef, useState } from 'react';
 import { PDFDocument, rgb, StandardFonts } from 'pdf-lib';
 import { read, utils, write } from 'xlsx';
 import JSZip from 'jzip';
@@ -9,49 +9,33 @@ export default function Home() {
   const pdfFiles = useRef(null);
   const workbook = useRef(null);
   const [showDownload, setShowDownload] = useState(false);
-  const [showMagic, setShowMagic] = useState(false);
-  // useEffect(() => {
-  //   fetch('/api/hello')
-  //     .then((res) => res.json())
-  //     .then((data) => {
-  //       console.log(data);
-  //     })
-  // }, [])
+  const [showLoading, setShowLoading] = useState(false);
+  const [starting, setStarting] = useState(1);
 
-  useEffect(() => {
-    console.log(pdfFiles)
-    console.log(workbook)
-    if (pdfFiles.current && workbook.current) {
-      setShowMagic(true);
-    } else {
-      setShowMagic(false);
-    }
-  }, [pdfFiles, workbook])
-
-  function handleTagging() {
-    if (workbook.current) {
+  function markExcel(modifiedPdfs) {
+    if (workbook.current && modifiedPdfs?.length > 0) {
       const wb = workbook.current;
       const ws = wb.Sheets[wb.SheetNames[0]]
-      const data = utils.sheet_to_json(ws, { header:"A" });
+      const data = utils.sheet_to_json(ws, { header: "A" });
       // console.log("data before:", data);
-
+      console.log(data)
       data.forEach((row, index) => {
         if (row.I && typeof row.I === "string") {
           const searchFor = row.I.trim();
-          pdfFiles.current.forEach((pdf, pdfIndex) => {
-            if (searchFor === pdf.fileName.trim()
-              || searchFor === pdf.fileName.trim().split(".pdf")[0]) {
-                pdfFiles.current[pdfIndex].foundACell = true;
+          modifiedPdfs.forEach((pdf, pdfIndex) => {
+            if (searchFor === pdf.name.trim()
+              || searchFor === pdf.name.trim().split(".pdf")[0]) {
+                modifiedPdfs[pdfIndex].foundACell = true;
               data[index].J = pdf.id;
-              // console.log(`Added ${pdf.id} to ${pdf.fileName} on cell J${index+1}`);
+              // console.log(`Added ${pdf.id} to ${pdf.name} on cell J${index+1}`);
               // console.log(`${pdfIndex + 1} pdfs of ${pdfFiles.current.length} added to excel`)
             }
           })
         }        
       });
       let withoutCells = ""
-      pdfFiles.current.forEach((pdf, i) => {
-        if (!pdf.foundACell) withoutCells += `\n - ${pdf.fileName}`
+      modifiedPdfs.forEach((pdf, i) => {
+        if (!pdf.foundACell) withoutCells += `\n - ${pdf.name}`
       })
       if (withoutCells !== "") {
         alert(`The following invoices did not have a reference in the excel file: ${withoutCells}`)
@@ -59,11 +43,61 @@ export default function Home() {
       const moddedSheet = utils.json_to_sheet(data);
       wb.Sheets[wb.SheetNames[0]] = moddedSheet;
       workbook.current = wb;
-  
-        // compressFiles();
-      setShowDownload(true);
-      setShowMagic(false);
+      // compressFiles();
+      // setShowDownload(true);
+      // setShowMagic(false);
+    } else {
+      alert("Something went wrong");
     }
+  }
+
+  async function modifyPdf({ arrayBuffer, file: { name } }, idNum) {
+    const pdfDoc = await PDFDocument.load(arrayBuffer)
+    const font = await pdfDoc.embedFont(StandardFonts.TimesRoman)
+    const pages = pdfDoc.getPages()
+    const firstPage = pages[0]
+    // const index = 73; // parseInt(i) + 1;
+    const id = String(idNum).padStart(3, '0');
+    const { height } = firstPage.getSize()
+    const fontSize = 30
+    firstPage.drawText(id, {
+      font,
+      x: 0,
+      y: height - fontSize,
+      size: 25,
+      color: rgb(0, 0, 0),
+    });
+    const pdfBytes = await pdfDoc.save()
+    return {
+      id,
+      name,
+      blob: new Blob([pdfBytes], { type: "application/pdf" }),
+      binary: pdfBytes,
+      foundACell: false,
+    };
+  }
+
+  function download(blob) {
+    const link = document.createElement('a');
+    link.href = window.URL.createObjectURL(blob);
+    link.download = "taggedInvoices.zip";
+    link.click();
+  }
+
+  function compressFiles(pdfArray) {
+    const zip = new JSZip();
+    pdfArray.forEach((file) => {
+      // console.log("adding to zip files:", file.name)
+      zip.folder("invoices").file(file.name, file.binary, { binary: true })
+    });
+    const wb = workbook.current;
+    if (wb) {
+      const excelFile = write(wb, { type: "binary" });
+      // console.log("zipping excel file:", "expenses.xlsx")
+      zip.file("expenses.xlsx", excelFile, { binary: true });
+    }
+    setShowLoading(false);
+    download(zip.generate({type:"blob"}));
   }
 
   function handleExcel(e) {
@@ -72,10 +106,6 @@ export default function Home() {
     reader.onload = (event) => {
       const bstr = event.target.result;
       workbook.current = read(bstr);
-
-      if (pdfFiles.current) {
-        setShowMagic(true);
-      }
     }
     reader.readAsArrayBuffer(file);
   }
@@ -89,9 +119,8 @@ export default function Home() {
         const reader = new FileReader();
         reader.onload = async (e) => {
           if (e.target.result) {
-            const modified = await modifyPdf(e.target.result, i, file.name);
-            // console.log(`labelled ${file.name} as ${modified.id}`)
-            fileArr.push(modified);
+            console.log(file)
+            fileArr.push({ arrayBuffer: e.target.result, file });
             if (fileArr.length === files.length) {
               resolve(fileArr);
             }
@@ -101,59 +130,40 @@ export default function Home() {
       })
     })
     promise.then(function(fileArrResult){
-      pdfFiles.current = fileArrResult;
-
-      if (workbook.current) {
-        setShowMagic(true);
-      }
-      // compressFiles(fileArrResult)
+      const sorted = fileArrResult.sort((a, b) => {
+        if (a.file.lastModified < b.file.lastModified) {
+          return -1;
+        }
+        if (a.file.lastModified > b.file.lastModified) {
+          return 1;
+        }
+        return 0;
+      })
+      pdfFiles.current = sorted;
+      setShowDownload(true);
     });
   };
-
-  async function modifyPdf(existingPdfBytes, i, fileName) {
-    const pdfDoc = await PDFDocument.load(existingPdfBytes)
-    const font = await pdfDoc.embedFont(StandardFonts.TimesRoman)
-    const pages = pdfDoc.getPages()
-    const firstPage = pages[0]
-    const index = parseInt(i) + 1;
-    const id = String(index).padStart(3, '0');
-    const { height } = firstPage.getSize()
-    const fontSize = 30
-    firstPage.drawText(id, {
-      font,
-      x: 5 + fontSize,
-      y: height - fontSize - 5,
-      size: 25,
-      color: rgb(0, 0, 0),
-    });
-    const pdfBytes = await pdfDoc.save()
-    return {
-      id,
-      fileName,
-      blob: new Blob([pdfBytes], { type: "application/pdf" }),
-      binary: pdfBytes,
-      foundACell: false,
-    };
+  
+  function handleNum(e) {
+    if (e.target.value) {
+      setStarting(e.target.value);
+    }
   }
 
-  function download(blob, fileName) {
-    const link = document.createElement('a');
-    link.href = window.URL.createObjectURL(blob);
-    link.download = fileName;
-    link.click();
-  }
+  async function handleDownload() {
+    setShowLoading(true);
+    const modifiedPdfs = []
+    if (pdfFiles.current) {
+      await Promise.all(pdfFiles.current.map(async (currentPdf, index) => {
+        const modifiedPdf = await modifyPdf(currentPdf, index + starting);
+        modifiedPdfs.push(modifiedPdf);
+      }));
+    }
 
-  function compressFiles() {
-    const zip = new JSZip();
-    pdfFiles.current.forEach((file) => {
-      // console.log("adding to zip files:", file.fileName)
-      zip.folder("invoices").file(file.fileName, file.binary, { binary: true })
-    });
-    const wb = workbook.current;
-    const excelFile = write(wb, { type: "binary" });
-    // console.log("zipping excel file:", "expenses.xlsx")
-    zip.file("expenses.xlsx", excelFile, { binary: true });
-    download(zip.generate({type:"blob"}), "taggedInvoices.zip");
+    if (workbook.current) {
+      markExcel(modifiedPdfs);
+    }
+    compressFiles(modifiedPdfs);
   }
 
   return (
@@ -165,26 +175,53 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <div className='input-container'>
-          <label htmlFor="excel-upload">Excel:&nbsp;</label>
+        {showLoading && <p className="loading">Doing things...</p>}
+        <div className="form">
+        <div>
+          <label htmlFor="excel-upload">Excel (optional):&nbsp;</label>
           <input onChange={handleExcel} type="file" id="excel-upload" name="excel-upload"
             accept=".csv, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel" />
         </div>
-        <div className='input-container'>
+        <div>
           <label htmlFor="file-upload">PDFs:&nbsp;</label>
           <input onChange={handlePdfs} type="file" id="file-upload" name="file-upload"
            multiple accept=".pdf" />
         </div>
-        {showMagic && <div className='input-container'>
-          <button onClick={handleTagging} className='magic-button'>Do magic</button>
+        <div>
+          <label htmlFor="number-select">Starting ID:&nbsp;</label>
+          <input onChange={handleNum} value={starting} type="number" id="number-select" name="number-select" />
+        </div>
+        {showDownload && <div>
+          <button onClick={handleDownload} className='magic-button dl'>Download Zip</button>
         </div>}
-        {showDownload && <div className='input-container'>
-          <button onClick={compressFiles} className='magic-button dl'>Download Zip</button>
-        </div>}
+        </div>
       </main>
       <style jsx>{`
-        .input-container {
-          margin-top: 30px;
+        .main {
+          border: 1px solid white;
+          min-height: 100vh;
+          padding: 4rem 0;
+          flex: 1;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+
+        @media (prefers-color-scheme: dark) {
+          .main {
+            border: 1px solid black;
+          }
+        }
+        .form {
+          border: 1px solid white;
+          padding: 10px;
+          display: grid;
+          row-gap: 30px;
+        }
+        .loading {
+          position: absolute;
+          top: 0;
         }
         .magic-button {
           background-color: blue;
